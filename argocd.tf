15b156e39f4aa97b9baacac1e892edc01ac04854
feriahmad <feri.ahmaddd@gmail.com>
2025-08-02 23:29:12 +0700

    chore: fix provider kubernetes

--- argocd.tf
+++ argocd.tf
@@ -3,7 +3,7 @@ resource "helm_release" "argocd" {
   namespace        = "argocd"
   create_namespace = true
 
-  repository = "argo"
+  repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   version    = "8.2.5"
 
